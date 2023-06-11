5372a9299e5c89d299062de19b3494c23a77272e
jaison10 <jaisonj1010@gmail.com>
2023-06-11 13:27:33 +0530

    models

---
+++ StudentAdminPortal-API/StudentAdminPortal-API/Models/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentAdminPortal_API.Models
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string PhysicalAddress { get; set; }
+        public string PostalAddress { get; set; }
+        //nav property
+        public Guid StudentID { get; set; }
+    }
+}
---
+++ StudentAdminPortal-API/StudentAdminPortal-API/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentAdminPortal_API.Models
+{
+    public class Student
+    {
+        [Key]
+        public  Guid Id { get; set; }
+        public string firstname { get; set; }    
+        public string lastname { get; set; } 
+        public DateTime DOB { get; set; }  
+        public string Email { get; set; }
+        public long Mobile { get; set; }   
+        public string ProfileImgUrl { get; set; }
+        public Guid GenderID { get; set; }
+        //Navigation Properties
+        public Gender Gender { get; set; }
+        public Address Address { get; set;}
+
+    }
+}
